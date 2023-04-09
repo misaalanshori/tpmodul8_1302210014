424c5c24318436af8d72c40c7842130f7c819855
M Isa Al Anshori <isabungsu@gmail.com>
2023-04-09 12:32:31 +0700

    menyelesaikan program dengan runtime configuration

---
+++ AppConfig.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302210014
+{
+    public class AppConfig
+    {
+        public CovidConfig config;
+
+        public const string configFile = @"./covid_config.json";
+
+        public AppConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+                Console.WriteLine("Membaca Config dari JSON");
+            } 
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Membuat Config Default");
+                setDefault();
+                WriteConfigFile();
+            }
+            
+        }
+
+        public CovidConfig ReadConfigFile()
+        {
+            string jsonText = File.ReadAllText(configFile);
+            config = JsonSerializer.Deserialize<CovidConfig>(jsonText);
+            return config;
+        }
+
+        public void setDefault()
+        {
+            config = new CovidConfig("celcius", 
+                                     14, 
+                                     "Anda tidak diperbolehkan masuk ke daalm gedung ini", 
+                                     "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+                                     );
+        }
+
+        public void WriteConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions() { 
+                WriteIndented = true
+            };
+            string jsonText = JsonSerializer.Serialize<CovidConfig>(config, options);
+            File.WriteAllText(configFile, jsonText);
+
+        }
+
+        public void UbahSatuan()
+        {
+            if (config.satuan_suhu.Equals("celcius"))
+            {
+                config.satuan_suhu = "fahrenheit";
+            } else if (config.satuan_suhu.Equals("fahrenheit"))
+            {
+                config.satuan_suhu = "celcius";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,9 +2,58 @@
 {
     internal class Program
     {
+        static void cekSuhu(AppConfig conf)
+        {
+            Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + conf.config.satuan_suhu);
+            float suhu;
+            try
+            {
+                suhu = float.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                suhu = 0;
+            }
+            Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+            int hari;
+            try
+            {
+                hari = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                hari = 0;
+            }
+
+            if (conf.config.satuan_suhu.Equals("celcius"))
+            {
+                if (suhu >= 36.5 && suhu <= 37.5 && hari < conf.config.batas_hari_demam)
+                {
+                    Console.WriteLine(conf.config.pesan_diterima);
+                }
+                else
+                {
+                    Console.WriteLine(conf.config.pesan_ditolak);
+                }
+            }
+            else if (conf.config.satuan_suhu.Equals("fahrenheit"))
+            {
+                if (suhu >= 97.7 && suhu <= 99.5 && hari < conf.config.batas_hari_demam)
+                {
+                    Console.WriteLine(conf.config.pesan_diterima);
+                }
+                else
+                {
+                    Console.WriteLine(conf.config.pesan_ditolak);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AppConfig conf = new AppConfig();
+            cekSuhu(conf);
+            conf.UbahSatuan();
+            cekSuhu(conf);
         }
     }
 }
